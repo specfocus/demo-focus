1be1ea5e2ff00525070b7ce26370ee08709a96e3
Lucas Oromi <lucas.oromi@kyndryl.com>
2022-04-15 20:47:50 -0600

    forwarding proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /* https://nextjs.org/docs/api-reference/next.config.js/rewrites
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*' // Proxy to Backend
+      }
+    ]
+  }
+  */
 }
-
 module.exports = nextConfig

6b80439ca78fca97513c34fea5c718777ff7a0ad
Lucas Martinez <lucas.oromi@gmail.com>
2022-05-11 10:00:44 -0600

    assert

--- lib/next/mongo/context/client.ts
+++ lib/next/mongo/context/client.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from 'mongodb';
-import assert from 'node:assert';
+import assert from 'assert';
 import performance from '../preformance';
 import MongoCollectionContext from './collection';
 
